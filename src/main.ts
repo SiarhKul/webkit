b90a3238c9c75d78be37ef876d1b5721326839f6
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-10-23 08:08:29 +0200

    chore: Add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
-import type {Application, Request, Response} from "express";
+import type { Application, Request, Response } from 'express'
 
 const express = require('express')
 
-const app:Application = express()
+const app: Application = express()
 const port = 3000
 
-
-app.get('/',( req: Request, res: Response  )=>{
-    res.send("hi")
+app.get('/', (req: Request, res: Response) => {
+    res.send('hi')
 })
 
-
-
-app.listen(3000,()=>{
-    console.log("Listening port is 3000")
+app.listen(port, () => {
+    console.log('Listening port is 3000')
 })
