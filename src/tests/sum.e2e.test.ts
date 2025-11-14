0fc1155a96990d069e57dcc8337aef02b2e3804c
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-11-14 21:42:46 +0100

    chore: Test throw

--- src/tests/sum.e2e.test.ts
+++ src/tests/sum.e2e.test.ts
@@ -8,6 +8,8 @@ import { UserRepository } from '../repositories/UserRepository'
 import { createUserData } from '../helpers/user-factory'
 import { Positions, Roles } from '../types/enums/index'
 import { User } from '../entity/User'
+import { AppError } from '../sharable/AppError'
+import { ErrorCodes } from '../sharable/jsend/ErrorCodes'
 
 describe('UserRepository', () => {
   let testDataSource: DataSource
@@ -36,6 +38,29 @@ describe('UserRepository', () => {
       })
 
       expect(createdUser.email).toEqual(user1.email)
+    })
+
+    it('should throw AppError 409 DUPLICATE_DATA on duplicate email', async () => {
+      const user1 = createUserData({
+        firstName: 'Alice',
+        lastName: 'Smith',
+        email: 'alice@example.com',
+        role: Roles.USER,
+        position: Positions.QA,
+      })
+      const user2 = createUserData({
+        firstName: 'Alice',
+        lastName: 'Smith',
+        email: 'alice@example.com',
+        role: Roles.USER,
+        position: Positions.QA,
+      })
+
+      await UserRepository.sighIn(user1)
+
+      await expect(
+        async () => await UserRepository.sighIn(user2)
+      ).rejects.toThrowError(/^duplicate key value violates unique constraint/)
     })
   })
 
