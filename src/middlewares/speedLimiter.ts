ddfc1286dc5723f9b073a1add064c6596cd61aff
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-11-18 21:21:21 +0100

    chore: Enhance slow-limiter

--- src/middlewares/speedLimiter.ts
+++ src/middlewares/speedLimiter.ts
@@ -1,8 +1,12 @@
 import slowDown from 'express-slow-down'
+import { Request, Response } from 'express'
 
 export const speedLimiter = slowDown({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  delayAfter: 100, // allow 100 requests per 15 minutes, then...
-  delayMs: 500, // begin adding 500ms of delay per request above 100:
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  delayAfter: 2, // allow 100 requests per 15 minutes, then...
+  delayMs: (hits) => hits * 300, // begin adding 500ms of delay per request above 100:
   maxDelayMs: 10000, // Will not increase delay beyond 10 seconds.
+  message: (req: Request, res: Response) => {
+    console.log('----------', req)
+  },
 })
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,12 +5,14 @@ import { limiter } from '../middlewares/rateLimiter'
 import { speedLimiter } from '../middlewares/speedLimiter'
 const userRouter = Router()
 
-// userRouter.use(limiter)
-userRouter.use(speedLimiter)
-
 userRouter.get('/user', limiter, UserController.getAllUsers)
 userRouter.post('/user', UserController.sighIn)
-userRouter.get('/user/:id', validateParameter('id'), UserController.getUserById)
+userRouter.get(
+  '/user/:id',
+  speedLimiter,
+  validateParameter('id'),
+  UserController.getUserById
+)
 userRouter.put('/user/:id', validateParameter('id'), UserController.updateUser)
 userRouter.delete(
   '/user/:id',
